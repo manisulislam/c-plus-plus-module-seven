fd198bfc11898dd74abd8c28894441ef0685a5e0
anisul islam <anisulislam980340@gmail.com>
2023-06-01 07:50:07 +0600

    object max and min

---
+++ object-max-min.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+    public:
+        string name;
+        int roll;
+        int marks;
+};
+int main(){
+    Student a[3];
+    for(int i=0; i<3; i++){
+        getline(cin, a[i].name);
+        cin>>a[i].marks>>a[i].roll;
+        cin.ignore();
+    }
+    Student mx;
+    mx.marks=INT_MIN;
+    for(int i=0; i<3; i++){
+        if(a[i].marks>mx.marks){
+            mx=a[i];
+        }
+    }
+    cout<<mx.name<<" "<<mx.roll<<" "<<mx.marks<<endl;
+    //minmum sort
+    Student mn;
+    mn.marks=INT_MAX;
+    for(int i=0; i<3; i++){
+        if(a[i].marks<mn.marks){
+            mn=a[i];
+        }
+    }
+    cout<<mn.name<<" "<<mn.roll<<" "<<mn.marks<<endl;
+
+    return 0;
+}
